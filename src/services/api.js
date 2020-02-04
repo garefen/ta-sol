247e806cade175eb3f203809d764e8900d5d6f6b
Garefen <gabrielbsarti@gmail.com>
2020-02-04 19:45:56 -0300

    setted deploy to heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+require('dotenv/config');
+
+
 const api = axios.create({
-    baseURL: 'https://api.openweathermap.org/data/2.5/forecast?id=3448433&APPID=8f34ff6349e06c9ef90bb986c86ab9b4'
+    baseURL: process.env.API_URL
 });
 
 export default api;
